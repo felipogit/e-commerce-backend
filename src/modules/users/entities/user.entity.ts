c92790914fcccb49427925642498f3a2d0e1ec1c
felipogit <felipeesped0023@gmail.com>
2024-07-02 10:21:34 -0300

    feat: insert migration

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,6 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity('users')
+@Entity('user')
 export class UserEntity {
     @PrimaryGeneratedColumn('rowid')
     id: number;
@@ -21,4 +21,13 @@ export class UserEntity {
 
     @Column({name: 'password', nullable: false})
     password: string;
+
+    @Column({name: 'type_user', nullable: false, default: 1})
+    type_user: number;
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date;
+
+    @UpdateDateColumn({name:'updated_at'})
+    updatedAt: Date;
 }
